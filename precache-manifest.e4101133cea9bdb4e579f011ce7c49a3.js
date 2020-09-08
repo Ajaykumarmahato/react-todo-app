90f57d395d4435c0107a8d63a69c273ef1d3de07
Ajaykumarmahato <mahatoa632@gmail.com>
2020-09-08 13:44:31 +0545

    Updates

---
+++ precache-manifest.e4101133cea9bdb4e579f011ce7c49a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8268e96e16b127de275f71c99b0218be",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "a5a22d43987d72707f76",
+    "url": "/react-todo-app/static/css/main.6dd5dedf.chunk.css"
+  },
+  {
+    "revision": "68095d12ea1e9f36f04a",
+    "url": "/react-todo-app/static/js/2.5a4d32dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.5a4d32dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a22d43987d72707f76",
+    "url": "/react-todo-app/static/js/main.ad674bcc.chunk.js"
+  },
+  {
+    "revision": "020d201bdbd86d7ba471",
+    "url": "/react-todo-app/static/js/runtime-main.eb6010f8.js"
+  }
+]);
